9214eeb8f82440f56a89157a07dca09436a11e1f
sarath <sarath@saraths-MacBook-Pro.local>
2023-05-07 19:26:53 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,133 @@
+import { GestureRecognizer, FilesetResolver } from "https://cdn.skypack.dev/@mediapipe/tasks-vision@0.1.0-alpha-13";
+const demosSection = document.getElementById("demos");
+let gestureRecognizer;
+let runningMode = "IMAGE";
+let enableWebcamButton;
+let webcamRunning = false;
+let serialConnected = false;
+const videoHeight = "360px";
+const videoWidth = "480px";
+// Before we can use HandLandmarker class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment to
+// get everything needed to run.
+const createGestureRecognizer = async () => {
+    const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.1.0-alpha-13/wasm");
+    gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: "/gesture_recognizer.task"
+        },
+        runningMode: runningMode
+    });
+    demosSection.classList.remove("invisible");
+};
+createGestureRecognizer();
+
+const encoder = new TextEncoder();
+const video = document.getElementById("webcam");
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+const gestureOutput = document.getElementById("gesture_output");
+const enableSerialButton = document.getElementById("serialButton");
+// Check if webcam access is supported.
+function hasGetUserMedia() {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+let port;
+
+const enableSerial = async () => {
+    if (serialConnected === true) {
+        serialConnected = false;
+        enableSerialButton.innerText = "ENABLE SERIAL";
+    } else {
+        serialConnected = false;
+        enableSerialButton.innerText = "DISABLE SERIAL";
+    }
+
+    port = await navigator.serial.requestPort();
+    await port.open({ baudRate: 9600 });
+}
+// If webcam supported, add event listener to button for when user
+// wants to activate it.
+if (hasGetUserMedia()) {
+    enableWebcamButton = document.getElementById("webcamButton");
+    enableWebcamButton.addEventListener("click", enableCam);
+}
+else {
+    console.warn("getUserMedia() is not supported by your browser");
+}
+
+enableSerialButton.addEventListener("click", enableSerial);
+// Enable the live webcam view and start detection.
+function enableCam(event) {
+    if (!gestureRecognizer) {
+        alert("Please wait for gestureRecognizer to load");
+        return;
+    }
+    if (webcamRunning === true) {
+        webcamRunning = false;
+        enableWebcamButton.innerText = "ENABLE PREDICTIONS";
+    }
+    else {
+        webcamRunning = true;
+        enableWebcamButton.innerText = "DISABLE PREDICITONS";
+    }
+    // getUsermedia parameters.
+    const constraints = {
+        video: true
+    };
+    // Activate the webcam stream.
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+        video.srcObject = stream;
+        video.addEventListener("loadeddata", predictWebcam);
+    });
+}
+let writer;
+async function predictWebcam() {
+    const webcamElement = document.getElementById("webcam");
+    // Now let's start detecting the stream.
+    if (runningMode === "IMAGE") {
+        runningMode = "VIDEO";
+        await gestureRecognizer.setOptions({ runningMode: "VIDEO" });
+    }
+    if (serialConnected) {
+        writer = port.writable.getWriter();
+        console.log(writer);
+    }
+    let nowInMs = Date.now();
+    const results = gestureRecognizer.recognizeForVideo(video, nowInMs);
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasElement.style.height = videoHeight;
+    webcamElement.style.height = videoHeight;
+    canvasElement.style.width = videoWidth;
+    webcamElement.style.width = videoWidth;
+    if (results.landmarks) {
+        for (const landmarks of results.landmarks) {
+            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+                color: "#00FF00",
+                lineWidth: 5
+            });
+            drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 });
+        }
+    }
+    canvasCtx.restore();
+    if (results.gestures.length > 0) {
+        gestureOutput.style.display = "block";
+        gestureOutput.style.width = videoWidth;
+        const categoryName = results.gestures[0][0].categoryName;
+        console.log(categoryName);
+        if (serialConnected) {
+            await writer.write(encoder.encode(categoryName));
+        }
+        const categoryScore = parseFloat(results.gestures[0][0].score * 100).toFixed(2);
+        gestureOutput.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %`;
+    }
+    else {
+        gestureOutput.style.display = "none";
+    }
+    // Call this function again to keep predicting when the browser is ready.
+    if (webcamRunning === true) {
+        window.requestAnimationFrame(predictWebcam);
+    }
+}
